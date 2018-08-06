f86243795135c2a9e61462059f01c1b0383a7c02
Joel Caleb <joel.caleb.pa@gmail.com>
2018-08-05 20:01:39 -0500

    Working

--- src/main/java/com/caleb/project/controller/ProductoRestController.java
+++ src/main/java/com/caleb/project/controller/ProductoRestController.java
@@ -5,21 +5,23 @@ package com.caleb.project.controller;
 
 import java.util.List;
 
+import com.caleb.project.entity.Producto;
+import com.caleb.project.repository.ProductoRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.caleb.project.entity.Producto;
-import com.caleb.project.repository.ProductoRepository;
 
 /**
  * @author caleb
  *
  */
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoRestController {
--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,7 +1,11 @@
-angular.module('myApp', [])
-    .controller('ctrl', function ($scope, $http) {
-        $http.get('http://localhost:8080/api/productos')
-            .then(function (response) {
-                $scope.productos = response.data;
-            });
-    });
+(function () {
+    'use strict';
+
+    var App = angular.module('myApp', [])
+        .controller('ctrl', ['$scope', '$http', function ($scope, $http) {
+            $http.get('http://localhost:9090/api/productos')
+                .then(function (response) {
+                    $scope.productos = response.data;
+                });
+        }]);
+})();
